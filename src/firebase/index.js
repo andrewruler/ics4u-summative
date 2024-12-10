00a07cb1148aa2f0e526c4307d7ea300a358f1ca
unknown <342587292@TDSB.ON.CA>
2024-12-10 11:31:00 -0500

    firebasesetup

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
-
+const firebaseConfig = {
+    apiKey: "AIzaSyCelJdoZvr3DsPXc8CYflG2z1r9tJOwMaU",
+    authDomain: "culminating-e3122.firebaseapp.com",
+    projectId: "culminating-e3122",
+    storageBucket: "culminating-e3122.firebasestorage.app",
+    messagingSenderId: "344567857506",
+    appId: "1:344567857506:web:150d9c49a00f566dd12c3b"
+  };
 
 const config = initializeApp(firebaseConfig)
 const auth = getAuth(config);
